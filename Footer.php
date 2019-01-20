cd15efa63a15547e01e5910dfca627f027fe76e8
Mayeda Meraj <mayedameraj5@gmail.com>
2019-01-20 19:42:26 +0500

    Forget password responsive and validated.

---
+++ Footer.php
@@ -0,0 +1,35 @@
+<div id="footer">
+    <div class="footer">
+        <div class="widget-footer">
+            <div class="wrap-container">
+                <div class="widgets">
+                    <h4>Contact information</h4>
+                    <p> This is contact information so that you can touch with us is easily.</p>
+                    <p><strong>Phone:</strong>03054799126</p>
+                    <p><strong>Email</strong>ambermalik205@gmail.com</p>
+                </div>
+                <div class="widgets">
+                    <h4>Sign up for newspaper</h4>
+                    <p> if you want to get latest updates and news the please sign up for news the please sign up for news letter below:</p>
+                    <p><strong>Email</strong></p>
+                    <input type="text" class="subscribe-text"><br>
+                    <input type="text" value="Subscribe" class="subscribe-btn">
+                </div>
+                <div class="widgets">
+                    <h4>Follow on social media</h4>
+                    <li><a href="https://www.facebook.com"><span class="fab fa-facebook-square fa-2x "></span>Facebook</a></li>
+                    <li><a href="https://www.twitter.com"><span class="fab fa-twitter fa-2x"></span>Twitter</a></li>
+                    <li> <a href="https://www.instagram.com"><span class="fab fa-instagram fa-2x"></span>Instagram</a></li>
+                </div>
+                <div class="widgets">
+                    <h4>Recent comments</h4>
+                    <p><strong>Aazka</strong>This is awesome website</p>
+                    <p><strong>Amber</strong>very helpful tutorials</p>
+                </div>
+            </div>
+        </div>
+        <div class="main-footer">
+            <p>copyright &copy; 2018</p>
+        </div>
+    </div>
+</div>
